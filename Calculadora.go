4acbcc7e8f61292bfe97e4fc7f8740898f72a4c1
allexkid <allexsantosrn@gmail.com>
2022-01-10 11:17:36 -0300

    Criando display
    Criando estrutura display para seleção da opção desejada.

--- Calculadora.go
+++ Calculadora.go
@@ -4,8 +4,33 @@ import (
 	"fmt"
 )
 
-var idade int = 15
+var option int = 0
 
 func main() {
-	fmt.Println(idade)
+
+	for {
+		fmt.Println("Calculadora de geometria Plana e Espacial")
+		fmt.Println("(1) Triângulo equilátero")
+		fmt.Println("(2) Retângulo")
+		fmt.Println("(3) Quadrado")
+		fmt.Println("(4) Círculo")
+		fmt.Println("(5) Pirâmide com base quadrangular")
+		fmt.Println("(6) Cubo")
+		fmt.Println("(7) Paralelepípedo")
+		fmt.Println("(8) Esfera")
+		fmt.Println("(0) Sair")
+
+		fmt.Print("Digite a sua opção: ")
+		fmt.Scanf("%d", &option)
+
+		switch option {
+
+		}
+
+		if option == 0 {
+			fmt.Println("Encerrando programa...")
+			return
+		}
+	}
+
 }
