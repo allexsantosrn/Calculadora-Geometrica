c25ec44505dfc21e467301a51c3e2e7304d2d7d2
allexkid <allexsantosrn@gmail.com>
2022-01-10 16:50:34 -0300

    Update functions
    Atualizando funções para cálculo do triângulo e retângulo.

--- Calculadora.go
+++ Calculadora.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-var option int = 0
+var option int
 
 func main() {
 
@@ -21,16 +21,59 @@ func main() {
 		fmt.Println("(0) Sair")
 
 		fmt.Print("Digite a sua opção: ")
-		fmt.Scanf("%d", &option)
+		fmt.Scanln(&option)
 
 		switch option {
 
-		}
-
-		if option == 0 {
+		case 0:
 			fmt.Println("Encerrando programa...")
 			return
+
+		case 1:
+			var base, altura float32
+			fmt.Print("Digite o tamanho da base do triângulo: ")
+			fmt.Scanln(&base)
+			fmt.Print("Digite o tamanho da altura do triângulo: ")
+			fmt.Scanln(&altura)
+			area := trianguloArea(base, altura)
+			perimetro := trianguloPerimetro(base)
+			fmt.Println("Área do triângulo: ", area)
+			fmt.Println("Perímetro do triângulo: ", perimetro)
+			fmt.Print("\n")
+
+		case 2:
+			var base, altura float32
+			fmt.Print("Digite o tamanho da base do retângulo: ")
+			fmt.Scanln(&base)
+			fmt.Print("Digite o tamanho da altura do retângulo: ")
+			fmt.Scanln(&altura)
+			area := retanguloArea(base, altura)
+			perimetro := retanguloPerimetro(base, altura)
+			fmt.Println("Área do retângulo: ", area)
+			fmt.Println("Perímetro do retângulo: ", perimetro)
+			fmt.Print("\n")
+
+		default:
+			fmt.Println("Opção inválida. Selecione uma das opções disponíveis!")
+			fmt.Print("\n")
 		}
+
 	}
 
+}
+
+func trianguloArea(base float32, altura float32) float32 {
+	return (base * altura) / 2
+}
+
+func trianguloPerimetro(base float32) float32 {
+	return base * 3
+}
+
+func retanguloArea(base float32, altura float32) float32 {
+	return (base * altura)
+}
+
+func retanguloPerimetro(base float32, altura float32) float32 {
+	return base*2 + altura*2
 }
